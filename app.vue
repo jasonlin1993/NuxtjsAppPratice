521dd236a02814849e17bf626a888e5be9dabaf3
jasonlin1993 <ninjachen.lin@gmail.com>
2024-06-17 11:19:28 +0800

    eslint + prettier 環境建置

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>{{ title }} - {{ name }}</h1>
+  </div>
+</template>
+
+<script setup>
+const year = 2023;
+const title = `${year}  Nuxt.js 練習`;
+const name = 'Ryan';
+</script>
