55bb13fe4dcdbcbc79bbba3079204b3dfd98c202
jasonlin1993 <ninjachen.lin@gmail.com>
2024-07-01 15:34:42 +0800

    Tutorial 18 api secret

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,20 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
   devtools: { enabled: true },
+  runtimeConfig: {
+    apiSecret: '怎麼可以讓你知道呢 :p',
+    public: {
+      apiBase: '/api',
+    },
+  },
   imports: {
     dirs: ['stores'],
+  },
+
+  appConfig: {
+    theme: {
+      primaryColor: '#0ea5e9',
+      darkMode: false,
+    },
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,17 @@
     <div class="flex flex-col items-center">
       <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
       <div class="my-4 flex flex-col space-y-4">
-        <NuxtLink to="/counter">前往 /counter</NuxtLink>
-        <NuxtLink to="/show">前往 /show</NuxtLink>
+        <NuxtLink to="/config">前往 /config</NuxtLink>
       </div>
+
+      <p class="mt-4 text-2xl text-gray-600">theme.darkMode:</p>
+      <span class="mt-4 text-3xl font-semibold text-blue-400">{{ theme.darkMode }}</span>
     </div>
   </div>
 </template>
+
+<script setup>
+const appConfig = useAppConfig();
+
+const { theme } = appConfig;
+</script>
